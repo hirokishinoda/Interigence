3c67144823f132e0aa28d6bc1f2107686a880325
j15422 <j15422@fukuoka.j.kisarazu.ac.jp>
2019-06-14 10:09:38 +0900

    [追加]再急降下法のプログラムを追加

--- Gradient/gradient_main.py
+++ Gradient/gradient_main.py
@@ -2,7 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def f(a):
-    return (a+1.0)**2 * (a-1.0)**2
+    #return (a+1.0)**2 * (a-1.0)**2
+    return a**4 - 8 * a**3 + 18 * a**2 -11
+
+def grad_f(a):
+    #return 4 * (a**3 - a)
+    return 4 * a**3 - 24 * a**2 + 36*a
 
 def graphic_f():
     N = 2
@@ -12,6 +17,15 @@ def graphic_f():
     plt.plot(x,y)
     plt.show()
 
+def gradient_decent(eta, a_t, step):
+
+    for _ in range(step):
+        a_t -= eta * grad_f(a_t)
+
+        print("a = ",a_t)
+
+    return a_t
 
 if __name__ == '__main__':
     graphic_f()
+    gradient_decent(0.01, 2.0, 100)
